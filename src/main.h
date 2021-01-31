8b1fb6db1aa503e889c46d8fba6e0442e4186f02
andrejdurina <andrejdu.9@gmail.com>
2021-01-31 03:32:24 +0100

    Once again created new repository.

---
+++ src/main.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <time.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <X11/Xlib.h>
+
+#include "common/loadShaders/shader.hpp"
+#include "common/callbacks/callBacks.hpp"
+#include "common/mouseParGen/particles.hpp"
+#include "math/math.hpp"
+
